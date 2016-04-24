46ca1fb681547effe279534119fb4fd8c6f4164b
Christian Kniep <christian@qnib.org>
2016-04-24 11:31:29 +0200

    adjust start/restore scripts

--- opt/qnib/kibana4/bin/restore.sh
+++ opt/qnib/kibana4/bin/restore.sh
@@ -6,25 +6,19 @@ ES_HOST=${ES_HOST-elasticsearch.service.consul}
 cnt=0
 
 wait_for_srv elasticsearch
-sleep 5
 
-kibana_size=$(curl -s http://${ES_HOST}:9200/.kibana/_status|jq '.indices[".kibana"].index.size_in_bytes')
-
-if [ "Xnull" == "X$kibana_size" ];then
-
-    if [ -f /opt/qnib/kibana4/dumps/mapping.json ];then
-         elasticdump \
-             --type=mapping \
-             --input=/opt/qnib/kibana4/dumps/mapping.json \
-             --output=http://${ES_HOST}:9200/.kibana
-    fi
-
-    if [ -f /opt/qnib/kibana4/dumps/data.json ];then
-         elasticdump \
-             --type=data \
-             --input=/opt/qnib/kibana4/dumps/data.json \
-             --output=http://${ES_HOST}:9200/.kibana
-    fi
+curl -s -XDELETE  http://${ES_HOST}:9200/.kibana
+if [ -f /opt/qnib/kibana4/dumps/mapping.json ];then
+    elasticdump \
+         --type=mapping \
+         --input=/opt/qnib/kibana4/dumps/mapping.json \
+         --output=http://${ES_HOST}:9200/.kibana
+fi
+if [ -f /opt/qnib/kibana4/dumps/data.json ];then
+    elasticdump \
+        --type=data \
+        --input=/opt/qnib/kibana4/dumps/data.json \
+        --output=http://${ES_HOST}:9200/.kibana
 fi
 
 exit 0
